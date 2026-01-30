3b4121cb0066ea18f7d96c7c43c4afa9b20f903e
NicolasZanin <nicolas.z06@free.fr>
2026-01-30 13:14:00 +0100

    task: reduce time complexity in Two sum exercise

--- 1_TwoSum/twosum.py
+++ 1_TwoSum/twosum.py
@@ -1,6 +1,7 @@
 from typing import List
 
-# First version
+"""
+# First Solution
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         dico = {}
@@ -25,5 +26,42 @@ class Solution:
                 index1 = dico[key][0]
                 index2 = dico[target - key][0]
                 return [min(index1, index2), max(index1, index2)]
+
+        return []
+"""
+
+"""
+# Second Solution
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dico = {}
+
+        for i, num in enumerate(nums):
+            existing_number = dico.get(num)
+            other_number = dico.get(target - num)
+
+            if existing_number is not None:
+                if num * 2 == target:
+                    return [existing_number, i]
+            elif other_number is not None:
+                return [other_number, i]
+            else:
+                dico[num] = i
+
+        return []
+"""
+
+# Last Solution
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dico = {}
+
+        for i, num in enumerate(nums):
+            target_index = dico.get(target - num)
+
+            if target_index is not None:
+                return [target_index, i]
+            else:
+                dico[num] = i
 
         return []
