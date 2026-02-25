8b7319de00f3a870d0eba3fcdd7f4a9e14d9efad
NicolasZanin <nicolas.z06@free.fr>
2026-02-25 17:24:55 +0100

    task: improve memory of 'add binary' solution

--- 0067_Add_Binary/0067_add_binary.cpp
+++ 0067_Add_Binary/0067_add_binary.cpp
@@ -6,7 +6,7 @@ using namespace std;
 /**
  * First Solution
  */
-class Solution {
+/*class Solution {
     static char addBinary(const char a, int &increment) {
         if (increment == 1) {
             const int add = a - '0' + increment;
@@ -46,6 +46,54 @@ class Solution {
             for (int i = static_cast<int>(get.size()) - 1; i >= 0; i--) {
                 res.push_back(get[i]);
             }
+
+            return res;
+        }
+};*/
+
+/**
+ * Second Solution
+ */
+class Solution {
+    static char addBinary(const char a, int &carry) {
+        if (carry) {
+            const int A = a - '0';
+            const int add = A ^ carry;
+            carry &= a;
+            return static_cast<char>(add + '0');
+        }
+
+        return a;
+    }
+
+    public:
+        string addBinary(string a, string b) {
+            int i = static_cast<int>(a.size()) - 1;
+            int j = static_cast<int>(b.size()) - 1;
+            string &res(i > j? a : b);
+            int k = i > j? i : j;
+
+            int carry = 0;
+
+            while (i >= 0 && j >= 0) {
+                const int A = a[i--] - '0';
+                const int B = b[j--] - '0';
+
+                const int add = A ^ B ^ carry;
+                carry = carry & (A | B) | A & B;
+                res[k--] = static_cast<char>(add + '0');
+            }
+
+            while (i >= 0) {
+                res[k--] = addBinary(a[i--], carry);
+            }
+
+            while (j >= 0) {
+                res[k--] = addBinary(b[j--], carry);
+            }
+
+            if (carry == 1)
+                res.insert(res.begin(), '1');
 
             return res;
         }
