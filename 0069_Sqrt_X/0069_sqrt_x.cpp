bc4be79ffd76139bad4a80e4e1b7c1dab2002934
NicolasZanin <nicolas.z06@free.fr>
2026-02-26 14:51:23 +0100

    add: add second solution for 'sqrt(x)' problem
    add dichotomy approach

--- 0069_Sqrt_X/0069_sqrt_x.cpp
+++ 0069_Sqrt_X/0069_sqrt_x.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+/**
+ * First Solution by approximation
+ */
 class Solution {
     public:
         int mySqrt(int x) {
@@ -20,4 +23,33 @@ class Solution {
         }
 };
 
-int main() {}
+
+/**
+ * Second Solution by dichotomy
+ */
+/*class Solution {
+    public:
+        int mySqrt(int x) {
+            int left = 0, right = x;
+
+            while (left <= right) {
+                const int mid = left + (right - left) / 2;
+                const long long square = static_cast<long long>(mid) * mid;
+
+                if (square == x) {
+                    return mid;
+                }
+
+                if (square > x) {
+                    right = mid - 1;
+                }
+                else {
+                    left = mid + 1;
+                }
+            }
+
+            return right;
+        }
+};*/
+
+int main() { }
