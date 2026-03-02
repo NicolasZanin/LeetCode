bdaa94ea4cdd1cf9ba45babace3f0cd3b2441eef
Nicolas Zanin <nicolas.zanin@etu.univ-cotedazur.fr>
2026-03-02 17:01:24 +0100

    task: add another approach for '0083 remove duplicate from sorted list' problem
    add recuresive approach

--- 0083_Remove_Duplicates_From_Sorted_List/0083_remove_duplicate_from_sorted_list.cpp
+++ 0083_Remove_Duplicates_From_Sorted_List/0083_remove_duplicate_from_sorted_list.cpp
@@ -10,6 +10,25 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+/**
+ * Recursive Approach
+ */
+/*class Solution {
+    ListNode* deleteDuplicates(ListNode* previous, ListNode* current) {
+        if (current == nullptr)
+            return new ListNode(previous->val);
+
+        if (previous->val == current->val)
+            return deleteDuplicates(previous, current->next);
+
+        return new ListNode(previous->val, deleteDuplicates(current, current->next));
+    }
+
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        return head? deleteDuplicates(head, head->next) : nullptr;
+    }
+};*/
 
 /**
  * Loop Approach
